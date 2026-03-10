d0a531e6651934cfb9a83d31f36c00f1ddd82d1e
Anthony Lawrence <anthony@abc.com>
2026-03-10 01:13:33 +0000

    Gross outlier simulation and housekeeping, also setting up gitignore for simulation csvs

--- run_simulations.R
+++ run_simulations.R
@@ -4,20 +4,49 @@ library(furrr)
 library(purrr)
 library(dplyr)
 library(readr)
+library(glue)
 
-plan(multisession, workers = 8)
+normal_eff_outpath <- "data/normal_efficiency.csv"
 
-future_map(
-    seq_len(10000),
-    \(rep_i) map(c(50, 200, 500), \(n) {
-        sim <- normal_efficiency_sim(N_pts = n, small_var = 1)
-        tibble(rep = rep_i, N_pts = n, slope_mm = sim$bisquare_slope, slope_ols = sim$ols_slope)
-    }) %>% list_rbind(),
-    .options = furrr_options(seed = 0),
-    .progress = TRUE
-) %>%
-    list_rbind() %>%
-    write_csv("data/normal_efficiency.csv")
+
+plan(multisession, workers = parallelly::availableCores() - 1)
+
+if (!file.exists(normal_eff_outpath)) {
+    cat("Starting normal efficiency simulation...\n")
+    future_map(
+        seq_len(10000),
+        \(rep_i) map(c(50, 200, 500), \(n) {
+            sim <- normal_efficiency_sim(N_pts = n, small_var = 1)
+            tibble(rep = rep_i, N_pts = n, slope_mm = sim$bisquare_slope, slope_ols = sim$ols_slope)
+        }) %>% list_rbind(),
+        .options = furrr_options(seed = 0),
+        .progress = TRUE
+    ) %>%
+        list_rbind() %>%
+        write_csv(normal_eff_outpath)
+} else {
+    cat("Normal efficiency file found. Skipping...\n")
+}
+
+contamination_pct <- 40
+gross_outlier_outpath <- glue("data/gross_outlier_{contamination_pct}.csv")
+
+if (!file.exists(gross_outlier_outpath)) {
+    cat("Starting gross outlier simulation...\n")
+    future_map(
+        seq_len(10000),
+        \(rep_i) map(c(50, 200, 500), \(n) {
+            sim <- gross_outlier_sim(N_pts = n, small_var = 1, big_var = 100, contamination_proportion = contamination_pct / 100)
+            tibble(rep = rep_i, N_pts = n, slope_mm = sim$bisquare_slope, slope_ols = sim$ols_slope)
+        }) %>% list_rbind(),
+        .options = furrr_options(seed = 0),
+        .progress = TRUE
+    ) %>%
+        list_rbind() %>%
+        write_csv(gross_outlier_outpath)
+} else {
+    cat("Gross outlier file found. Skipping...\n")
+}
 
 
 plan(sequential)
--- simulations.R
+++ simulations.R
@@ -1,11 +1,12 @@
 source("functions.R")
 
+# note we are currently not returning the intercept term!
+
 normal_efficiency_sim <- function(N_pts, small_var = 1, estimator = "MM", initial_estimator = "S") {
-    
     stopifnot(N_pts > 0, small_var > 0)
-    
+
     if (estimator == "MM") {
-        weight_fn <- function(x) bisquare_weight(x, k=4.68)
+        weight_fn <- function(x) bisquare_weight(x, k = 4.68)
     } else if (estimator == "Huber") {
         initial_estimator <- "madn"
         weight_fn <- function(x) huber_weight(x)
@@ -13,18 +14,59 @@ normal_efficiency_sim <- function(N_pts, small_var = 1, estimator = "MM", initia
         stopifnot(1 == 0)
     }
 
-    x_vals = seq(1, N_pts)
-    
+    # Generate evenly-spaced x values, then y=x+N(0, small_var) and fit OLS vs MM-estimator
+    x_vals <- seq(1, N_pts)
+
     small_noise <- rnorm(n = N_pts, mean = 0, sd = sqrt(small_var))
-    y_vals = 1 * x_vals + small_noise
+    y_vals <- 1 * x_vals + small_noise
 
     bi_fit <- IRWLS_fit_simple(x_vals, y_vals, initial_est = initial_estimator, weight_fn = weight_fn)
     ols_fit <- lm(y_vals ~ x_vals)
 
-
+    # return the slope [2], [1] would be the intercept
     list(
         bisquare_slope = coef(bi_fit)[2],
         ols_slope      = coef(ols_fit)[2]
     )
 }
 
+
+gross_outlier_sim <- function(N_pts, small_var = 1, big_var = 100, contamination_proportion, estimator = "MM", initial_estimator = "S") {
+    # TODO: How to let the caller decide the big noise distribution?
+    # puts the job of making randomness on caller, current approach (randomness inside this function) more intuitive with furrr
+
+    stopifnot(N_pts > 0, small_var > 0, contamination_proportion <= 1, contamination_proportion > 0)
+
+    if (estimator == "MM") {
+        weight_fn <- function(x) bisquare_weight(x, k = 4.68)
+    } else if (estimator == "Huber") {
+        initial_estimator <- "madn"
+        weight_fn <- function(x) huber_weight(x)
+    } else {
+        stopifnot(1 == 0)
+    }
+
+    # this way with n_contaminates was chosen for easier file naming in run_simulations.R
+    n_contaminates <- floor(contamination_proportion * N_pts)
+
+    # Generate evenly spaced x values, then y+x+N(0, small_var)
+
+    x_vals <- seq(1, N_pts)
+    small_noise <- rnorm(n = N_pts, mean = 0, sd = sqrt(small_var))
+    big_noise <- rnorm(n = n_contaminates, mean = 0, sd = sqrt(big_var))
+
+    y_vals <- 1 * x_vals + small_noise
+
+    # Corrupt a proportion by adding noise to them and then fit OLS vs MM-estimator
+    contamination_idx <- sample(1:N_pts, size = n_contaminates)
+
+    y_vals[contamination_idx] <- y_vals[contamination_idx] + big_noise
+
+    bi_fit <- IRWLS_fit_simple(x_vals, y_vals, initial_est = initial_estimator, weight_fn = weight_fn)
+    ols_fit <- lm(y_vals ~ x_vals)
+
+    list(
+        bisquare_slope = coef(bi_fit)[2],
+        ols_slope      = coef(ols_fit)[2]
+    )
+}
