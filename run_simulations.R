cea69cc7d0e3aee86dc919d1676daf0090d00fc3
Anthony Lawrence <anthony@abc.com>
2026-03-17 01:08:12 +0000

    Breakdown point simulation, plots next

--- run_simulations.R
+++ run_simulations.R
@@ -17,7 +17,7 @@ if (!file.exists(normal_eff_outpath)) {
         seq_len(10000),
         \(rep_i) map(c(50, 200, 500), \(n) {
             sim <- normal_efficiency_sim(N_pts = n, small_var = 1)
-            tibble(rep = rep_i, N_pts = n, slope_mm = sim$bisquare_slope, slope_ols = sim$ols_slope, slope_mass = sim$mass_slope)
+            tibble(rep = rep_i, N_pts = n, slope_mm = sim$bisquare_slope, slope_ols = sim$ols_slope, intercept_mm = sim$bisquare_int, intercept_ols = sim$ols_int)
         }) %>% list_rbind(),
         .options = furrr_options(seed = 0),
         .progress = TRUE
@@ -37,7 +37,7 @@ if (!file.exists(gross_outlier_outpath)) {
         seq_len(10000),
         \(rep_i) map(c(50, 200, 500), \(n) {
             sim <- gross_outlier_sim(N_pts = n, small_var = 1, big_var = 100, contamination_proportion = contamination_pct / 100)
-            tibble(rep = rep_i, N_pts = n, slope_mm = sim$bisquare_slope, slope_ols = sim$ols_slope, slope_mass = sim$mass_slope)
+            tibble(rep = rep_i, N_pts = n, slope_mm = sim$bisquare_slope, slope_ols = sim$ols_slope, intercept_mm = sim$bisquare_int, intercept_ols = sim$ols_int)
         }) %>% list_rbind(),
         .options = furrr_options(seed = 0),
         .progress = TRUE
@@ -49,4 +49,21 @@ if (!file.exists(gross_outlier_outpath)) {
 }
 
 
+breakdown_outpath <- "data/breakdown.csv"
+if (!file.exists(breakdown_outpath)) {
+    cat("Starting breakdown simulation...\n")
+    future_map(
+        seq_len(1500),
+        \(rep_i) map(c(0.2, 0.45, 0.49, 0.51, 0.55), \(c_p) {
+            sim <- gross_outlier_sim(N_pts = 200, small_var = 1, big_mean = 10000, big_var = 1, contamination_proportion = c_p)
+            tibble(rep = rep_i, N_pts = 200, slope_mm = sim$bisquare_slope, slope_ols = sim$ols_slope, intercept_mm = sim$bisquare_int, intercept_ols = sim$ols_int, cp = c_p)
+        }) %>% list_rbind(),
+        .options = furrr_options(seed = 0),
+        .progress = TRUE
+    ) %>%
+        list_rbind() %>%
+        write_csv(breakdown_outpath)
+} else {
+    cat("Breakdown file found. Skipping...\n")
+}
 plan(sequential)
--- simulations.R
+++ simulations.R
@@ -23,18 +23,19 @@ normal_efficiency_sim <- function(N_pts, small_var = 1, estimator = "MM", initia
 
     bi_fit <- IRWLS_fit_simple(x_vals, y_vals, initial_est = initial_estimator, weight_fn = weight_fn)
     ols_fit <- lm(y_vals ~ x_vals)
-    mass_fit <- rlm(y_vals ~ x_vals, method= "MM", psi = psi.bisquare)
+    #mass_fit <- rlm(y_vals ~ x_vals, method= "MM", psi = psi.bisquare)
 
     # return the slope [2], [1] would be the intercept
     list(
         bisquare_slope = coef(bi_fit)[2],
         ols_slope      = coef(ols_fit)[2],
-        mass_slope     = coef(mass_fit)[2]
+        bisquare_int   = coef(bi_fit)[1],
+        ols_int        = coef(ols_fit)[1]
     )
 }
 
 
-gross_outlier_sim <- function(N_pts, small_var = 1, big_var = 100, contamination_proportion, estimator = "MM", initial_estimator = "S") {
+gross_outlier_sim <- function(N_pts, small_var = 1, big_mean = 0, big_var = 100, contamination_proportion, estimator = "MM", initial_estimator = "S") {
     # TODO: How to let the caller decide the big noise distribution?
     # puts the job of making randomness on caller, current approach (randomness inside this function) more intuitive with furrr
 
@@ -56,22 +57,22 @@ gross_outlier_sim <- function(N_pts, small_var = 1, big_var = 100, contamination
 
     x_vals <- seq(1, N_pts)
     small_noise <- rnorm(n = N_pts, mean = 0, sd = sqrt(small_var))
-    big_noise <- rnorm(n = n_contaminates, mean = 0, sd = sqrt(big_var))
+    big_noise <- rnorm(n = n_contaminates, mean = big_mean, sd = sqrt(big_var))
 
     y_vals <- 1 * x_vals + small_noise
 
     # Corrupt a proportion by adding noise to them and then fit OLS vs MM-estimator
     contamination_idx <- sample(1:N_pts, size = n_contaminates)
 
-    y_vals[contamination_idx] <- y_vals[contamination_idx] + big_noise
+    y_vals[contamination_idx] <- big_noise
 
     bi_fit <- IRWLS_fit_simple(x_vals, y_vals, initial_est = initial_estimator, weight_fn = weight_fn)
     ols_fit <- lm(y_vals ~ x_vals)
-    mass_fit <- rlm(y_vals ~ x_vals, method= "MM", psi = psi.bisquare)
 
     list(
         bisquare_slope = coef(bi_fit)[2],
         ols_slope      = coef(ols_fit)[2],
-        mass_slope     = coef(mass_fit)[2]
+        bisquare_int   = coef(bi_fit)[1],
+        ols_int        = coef(ols_fit)[1]
     )
 }
